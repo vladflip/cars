00144ce59da8cfed30f995d6614e7bd3455d7acb
flip <sef@sdf.fds>
2015-05-23 11:54:37 +0300

    changes

--- resources/views/pages/profile.blade.php
+++ resources/views/pages/profile.blade.php
@@ -1,1 +1,29 @@
 @extends('layouts.main')
+
+@section('body')
+
+	<div class="profile-content">
+		
+		<div class="container">
+			
+			<div class="p_left">
+				
+				<div class="p_header">
+					
+					<h3>Моя компания</h3>
+
+					<h4>Мой профиль</h4>
+
+					<div class="p_settings">
+						Настройки
+					</div>
+
+				</div>
+
+			</div>
+
+		</div>
+
+	</div>
+
+@stop
