787e1449cc99c11c121e780228ebcd035d3ba573
flip <sef@sdf.fds>
2015-06-02 14:03:13 +0300

    header changed

--- resources/views/inc/header.blade.php
+++ resources/views/inc/header.blade.php
@@ -2,40 +2,50 @@
 	
 	<div class="container">
 		
-		<div class="header_logo"></div>
-
-		<div class="header_title">
-			Справочник коммерческих организаций по обслуживанию авто и продажам запчастей.
+		<div>
+			<div class="header_logo"></div>
+			
+			<div class="header_title">
+				Справочник коммерческих организаций по обслуживанию авто и продажам запчастей.
+			</div>
 		</div>
 
-		@if(Request::is('profile'))
+		<div>
 
-			<div class="profile">
-				
-				<span>x</span>
-
-				<div class="profile_names">
-					<h5>Павел Калачов</h5>
-					<h5>Ренестранском</h5>
-				</div>
-
-				<div class="profile_ava">
-					<img src="img/ava.jpg" alt="">
-				</div>
-
-			</div>
-
-		@else
+			@if(Request::is('profile'))
 			
-			<div class="header_buttons">
-
-				<div class="header_signup">Регистрация</div>
+				<div class="profile">
+					
+					<span>x</span>
+			
+					<div class="profile_names">
+						<h5>Павел Калачов</h5>
+						<h5>Ренестранском</h5>
+					</div>
+			
+					<div class="profile_ava">
+						<img src="img/ava.jpg" alt="">
+					</div>
+			
+				</div>
+			
+			@else
 				
-				<div class="header_signin">Вход</div>
+				<div class="header_inputs">
+			
+					<input class="header_login" type="text" placeholder="Логин">
+					
+					<input class="header_pass" type="password" placeholder="Пароль">
 
-			</div>
+					<div class="header_submit-arrow"></div>
+			
+				</div>
 
-		@endif
+				<div class="header_register">Регистрация</div>
+			
+			@endif
+
+		</div>
 
 	</div>
 
