37e31e42c36b9a0ce4c76876f8593d165e1778c1
flip <sef@sdf.fds>
2015-06-04 17:23:03 +0300

    search bar changed

--- resources/views/inc/search.blade.php
+++ resources/views/inc/search.blade.php
@@ -1,34 +1,10 @@
-<div class="search-side-bar">
+<div class="search">
 					
-	<h3>Найти</h3>
+	<h3>Заказ запчастей</h3>
 
-	<h4>Тип</h4>
-	<select name="" id="">
-		<option value="">Грузовики</option>
-		<option value="">Автобусы</option>
-		<option value="">Малый коммерческий транспорт</option>
-		<option value="">Прицепы</option>
-	</select>
+	<p>Заполните форму и получите 
+		персональные предложения от актуальных компаний</p>
 
-	<h4>Производитель</h4>
-	<select name="" id="">
-		<option value="">Audi</option>
-	</select>
-
-	<h4>Модель</h4>
-	<select name="" id="">
-		<option value="">TT</option>
-	</select>
-
-	<h4>Год выпуска</h4>
-	<input type="text">
-
-	<input type="checkbox"> <label for="">Новая</label>
-	<input type="checkbox"> <label for="">б/у</label>
-
-	<h4>Дополнительно</h4>
-	<input type="text" class="s-s_more" value="VIN и т.п">
-
-	<div class="s-s_send">Отправить запрос</div>
+	<div class="search_button">Отправить запрос</div>
 
 </div>
