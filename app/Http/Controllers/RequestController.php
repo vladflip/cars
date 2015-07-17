22218afd5b84f73896ea6a3f8dd8d5bdb332d404
flip <my@email.com>
2015-07-17 15:40:50 +0300

    requests initial

---
+++ app/Http/Controllers/RequestController.php
@@ -0,0 +1,16 @@
+<?php namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class RequestController extends Controller {
+
+	public function create() {
+
+		return 'hello';
+
+	}
+
+}
--- app/Http/routes.php
+++ app/Http/routes.php
@@ -147,6 +147,12 @@ Route::group(['prefix' => 'api', 'middleware' => 'api'], function(){
 
 	});
 
+	Route::group(['prefix' => 'request', 'middleware' => 'auth'], function() {
+
+		post('create', ['uses' => 'RequestController@create']);
+
+	});
+
 	get('get-makes', 'APIController@makes');
 
 	get('get-makes-by-type-has-comps', 'APIController@makes_by_type_has_comps');
