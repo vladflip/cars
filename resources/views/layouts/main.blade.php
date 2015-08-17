a9f3f9cde0f80949ef295ca2488b054d6d33a805
vladflip <vlad.flip.prg@gmail.com>
2015-08-17 15:55:36 +0300

    update

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -10,6 +10,13 @@
 		<link rel="stylesheet" href="{{ URL::to('/') }}/css/vendor/popup.css">
 		<link rel="stylesheet" href="{{ URL::to('/') }}/css/vendor/jcrop.css">
 		<link rel="stylesheet" href="{{ URL::to('/') }}/css/vendor/quill.snow.css">
+		<meta http-equiv="cache-control" content="max-age=0" />
+		<meta http-equiv="cache-control" content="no-cache" />
+		<meta http-equiv="cache-control" content="no-store" />
+		<meta http-equiv="cache-control" content="must-revalidate" />
+		<meta http-equiv="expires" content="0" />
+		<meta http-equiv="expires" content="Tue, 01 Jan 1980 1:00:00 GMT" />
+		<meta http-equiv="pragma" content="no-cache" />
 	</head>
 
 	<body data-home="{{ URL::to('/') }}" data-csrf="{{ csrf_token() }}">
