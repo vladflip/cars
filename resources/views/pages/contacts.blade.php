0185d98f37cbd7b076bfdcea7d5f4af1338e04ab
flip <my@email.ru>
2015-09-08 12:22:45 +0300

    contacts layout

--- resources/views/pages/contacts.blade.php
+++ resources/views/pages/contacts.blade.php
@@ -9,7 +9,26 @@
 		<div class="container">
 			
 			<div class="contacts_content">
-				Для ваших вопросов и предложений работает служба технической поддержки:
+				<div class="contacts_header">
+					Для ваших вопросов и предложений работает служба технической поддержки:
+				</div>
+
+				<div>
+					<div class="popup_field">
+						<label class="popup_label">Имя</label>
+						<input type="text" class="popup_input">
+					</div>
+					<div class="popup_field">
+						<label class="popup_label">Почта</label>
+						<input type="text" class="popup_input">
+					</div>
+					<div class="popup_field">
+						<label class="popup_label">Сообщение</label>
+						<textarea id="" cols="50" rows="10" class="popup_textarea contacts_textarea"></textarea>
+					</div>
+				</div>
+
+				<div class="popup_button">Отправить</div>
 			</div>
 
 		</div>
