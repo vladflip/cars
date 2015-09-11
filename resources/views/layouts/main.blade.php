9edde07feedb99de97f844509d31d891ca23053c
Golovachov Anton <scoutrul@mail.ru>
2015-09-11 21:04:48 +0300

    METRIKA

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -45,6 +45,39 @@
 
 		</div>
 
+
+<!-- Yandex.Metrika counter -->
+<script type="text/javascript">
+    (function (d, w, c) {
+        (w[c] = w[c] || []).push(function() {
+            try {
+                w.yaCounter32453035 = new Ya.Metrika({
+                    id:32453035,
+                    clickmap:true,
+                    trackLinks:true,
+                    accurateTrackBounce:true,
+                    webvisor:true,
+                    trackHash:true
+                });
+            } catch(e) { }
+        });
+
+        var n = d.getElementsByTagName("script")[0],
+            s = d.createElement("script"),
+            f = function () { n.parentNode.insertBefore(s, n); };
+        s.type = "text/javascript";
+        s.async = true;
+        s.src = "https://mc.yandex.ru/metrika/watch.js";
+
+        if (w.opera == "[object Opera]") {
+            d.addEventListener("DOMContentLoaded", f, false);
+        } else { f(); }
+    })(document, window, "yandex_metrika_callbacks");
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/32453035" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+<!-- /Yandex.Metrika counter -->
+
+
 		<script src="{{ URL::to('/') }}/js/vendor/jquery.js"></script>
 		<script src="{{ URL::to('/') }}/js/vendor/jquery-ui.js"></script>
 		<script src="{{ URL::to('/') }}/js/vendor/photoset-grid.js"></script>
