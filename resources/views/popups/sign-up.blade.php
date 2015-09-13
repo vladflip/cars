5f5318b8c26788035f015ac17b47cf3943be0b1a
Golovachov Anton <scoutrul@mail.ru>
2015-09-13 17:15:06 +0300

    some text in reg

--- resources/views/popups/sign-up.blade.php
+++ resources/views/popups/sign-up.blade.php
@@ -2,8 +2,8 @@
 	
 	<div class="popup_content">
 		
-		<h3 class="popup_header">Регистрация в клубе "Комтранс"</h3>
-		<div class="popup_info">*Вам станет доступен специализированный поиск услуг и товаров для грузовых авто.</div>
+		<h3 class="popup_header">Регистрация<br>в клубе "Комтранс"</h3>
+		<div class="popup_info">*Вам станет доступен специализированный поиск товаров и услуг для грузовых авто.</div>
 
 		{!! Form::open(['method' => 'post', 'route' => 'user-create', 'id' => 'sign-up-form']) !!}
 
@@ -20,6 +20,13 @@
 				<label class="popup_label">Введите пароль</label>
 			
 				<input name="password" type="text" class="popup_input">
+			
+			</div>
+
+			<div>
+			
+				<input name="agree" type="checkbox" class="popup_agree">
+				<div>Согласен с <a href="">правилами</a> клуба.</div>
 			
 			</div>
 			
